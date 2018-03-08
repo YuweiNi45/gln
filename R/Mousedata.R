863ad6cfda7f137db0587bde745b20a6b52f2d2a
YuweiNi45 <yuweini45@gmail.com>
2018-03-07 20:22:58 -0500

    Upload the description of data

---
+++ R/Mousedata.R
@@ -0,0 +1,27 @@
+#'Mousedata: A data set about a diet induced obesity mouse study
+#'
+#'The Mousedata is an analyze data used to evaluate leves of urinary PGE-M and and PGD-M in obese vs. lean mice +/- celecoxib.
+#'
+#'@section Background:
+#'
+#'In a previous study by the investigator, levels of urinary PGE-M, an inflammation biomarker, were increased in association with obesity. COX-2, an enzyme in the related inflammation pathway,
+#'is elevated in inflamed adipose tissue in association with obesity. To test the hypothesis that COX-2 is directly responsible for the elevation in urinary PGE-M in obesity, Celecoxib, a selective COX-2 inhibitor was utilized in a mouse model of obesity. Mice were randomized into four groups as shown below:
+#'
+#'1. Low fat 10\% diet, sham OVX
+#'
+#'2. Low fat 10\% diet, sham OVX + celecoxib in diet
+#'
+#'3. High fat 60\% diet, OVX
+#'
+#'4. High fat 60\% diet, OVX + celecoxib in diet
+#'
+#'
+#' @details Following 9 days on control or celecoxib containing diets, mice were placed into metabolic cages (3-4 mice per cage) and urine was collected for up to 48 hours. 5 to 6 samples of urine from each of the four groups for a total of 23 samples were analyzed to quantify levels of urinary PGE-M and PGD-M along with
+#' associated creatinine levels in each sample.
+#'
+#' @section Mouse Background:
+#'
+#' @docType data
+#'
+#' @name Mousedata
+NULL
