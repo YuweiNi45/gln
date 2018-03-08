20102cb5e253e22bb50f9c8fa122c6b6690d3f71
YuweiNi45 <yuweini45@gmail.com>
2018-03-07 22:35:27 -0500

    Change the title

--- R/gln.R
+++ R/gln.R
@@ -1,4 +1,4 @@
-#' gln: A package for analysis of variance(ANOVA).
+#' gln: Numerical and Graphical Summary for Comparing Continuous Data in Multiple Groups
 #'
 #'The "gln" is a set of functions that analysis whether there is difference in any of the means among different groups in our dataset. This package is designed to make it easy to install and load multiple "ANOVA" functions in a single step. The “gln” packages includes parametric (F-Test) and non-parametric method (KW-Test). The output contains both tables and figures based on the result of ANOVA analysis.
 #'
